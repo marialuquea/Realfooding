011208ea79426ecb381d47ea6869b3a20bebf425
Maria Luque Anguita <mluqueanguita@gmail.com>
2018-10-07 12:51:44 +0100

    all pages are correctly directed, contact looks weird

--- app.py
+++ app.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
+@app.route('/realfood/')
+def realfood():
+    return render_template('realfood.html')
+
+@app.route('/mealplan/')
+def mealplan():
+    return render_template('mealplan.html')
+
+@app.route('/groceries/')
+def groceries():
+    return render_template('groceries.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
